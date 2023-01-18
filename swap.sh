aa8a9c4748543e235c26444149da0078191a1c21
zhlhlf <60917750+zhlhlf@users.noreply.github.com>
2023-01-18 21:33:12 +0800

    Update swap.sh

--- swap.sh
+++ swap.sh
@@ -1,7 +1,7 @@
 export SWAP_FILE=$(swapon --show=NAME | tail -n 1)
           sudo swapoff $SWAP_FILE
           sudo rm $SWAP_FILE
-          sudo fallocate -l 12G $SWAP_FILE
+          sudo fallocate -l ${swapsize}G $SWAP_FILE
           sudo chmod 600 $SWAP_FILE
           sudo mkswap $SWAP_FILE
           sudo swapon $SWAP_FILE
